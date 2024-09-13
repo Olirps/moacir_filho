90f4ad548d92da20837c768c4f09a5c560c49e39
olirps <olir.ps@gmail.com>
2024-09-13 15:32:24 +0000

    ajuste do time para expirar token

--- src/services/userService.js
+++ src/services/userService.js
@@ -33,7 +33,7 @@ const authenticateUser = async (username, password) => {
         const token = jwt.sign(
             { id: user.id, username: user.username },
             process.env.JWT_SECRET,
-            { expiresIn: '1h' }
+            { expiresIn: '4h' }
         );
         return { user, token };
     } catch (error) {
